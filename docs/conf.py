a818300239d449309132bb735139458f474dfcaf
Jose Carpio <jac2391@columbia.edu>
2020-10-15 17:25:06 -0500

    set up docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 import sphinx_rtd_theme
+from recommonmark.transform import AutoStructify
+
 project = 'patchwords'
 copyright = '2020, Jose Carpio'
 author = 'Jose Carpio'
@@ -28,11 +30,18 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)
+
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,12 +57,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ docs/index.md
@@ -0,0 +1,12 @@
+# Welcome to patchwords's documentation!
+
+## header
+
+-   these
+-   are
+-   bullets
+
+```eval_rst
+.. note::
+   We can still mix rst
+```
