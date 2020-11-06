7a932390984f04437d1fe10fa03d5fb15fa36a84
Jose Carpio <jac2391@columbia.edu>
2020-11-05 18:21:33 -0600

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.use(express.static(__dirname + '/'))
-const port = 8000
-app.listen(port)
-console.log('app on port' + port)
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(process.env.PORT || 8000)
