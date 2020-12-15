39638266549cc94f955a45c675a1dfb3c22729ce
Jose Carpio <jac2391@columbia.edu>
2020-12-15 14:18:51 -0600

    add drag to delete

--- scripts/dragactions.js
+++ scripts/dragactions.js
@@ -2,12 +2,9 @@
 /*eslint no-undef: "error"*/
 
 interact('.draggable').draggable({
-    // enable inertial throwing
-    inertia: true,
     // keep the element within the area of its parent
     modifiers: [
         interact.modifiers.restrictRect({
-            restriction: 'parent',
             endOnly: true,
         }),
     ],
@@ -37,3 +34,47 @@ function dragMoveListener(event) {
     target.setAttribute('data-x', x)
     target.setAttribute('data-y', y)
 }
+
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+    overlap: 0.1,
+
+    // listen for drop related events:
+    ondropactivate: function (event) {
+        // add active dropzone feedback
+        event.target.classList.add('drop-active')
+    },
+    ondragenter: function (event) {
+        var draggableElement = event.relatedTarget
+        var dropzoneElement = event.target
+
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target')
+        draggableElement.classList.add('can-drop')
+    },
+    ondragleave: function (event) {
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target')
+        event.relatedTarget.classList.remove('can-drop')
+    },
+    ondrop: function (event) {
+        console.log(event.relatedTarget)
+        document.getElementById('patch').removeChild(event.relatedTarget)
+    },
+    ondropdeactivate: function (event) {
+        // remove active dropzone feedback
+        event.target.classList.remove('drop-active')
+        event.target.classList.remove('drop-target')
+    },
+})
+
+interact('.drag-drop').draggable({
+    inertia: true,
+    modifiers: [
+        interact.modifiers.restrictRect({
+            endOnly: true,
+        }),
+    ],
+    autoScroll: true,
+    listeners: { move: dragMoveListener },
+})
