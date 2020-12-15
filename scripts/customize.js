3127fe6fc04bad92f5489be635c77b9c9dc49368
Jose Carpio <jac2391@columbia.edu>
2020-12-14 22:40:49 -0600

    add column picker

--- scripts/customize.js
+++ scripts/customize.js
@@ -2,6 +2,11 @@ function changeColor(newColor) {
     document.body.style.backgroundColor = newColor
 }
 
+function changeColumns(columns) {
+    document.getElementById('patch').style.columnCount = columns
+}
+
 module.exports = {
     changeColor,
+    changeColumns,
 }
